4939c3dab7713646eb693fddfa698c37559abb07
navsubrm <navsubrm@gmail.com>
2025-10-17 15:56:15 +0000

    Updated to run in Firebase Studio

---
+++ .idx/dev.nix
@@ -0,0 +1,29 @@
+{pkgs}: {
+  channel = "stable-24.05";
+  packages = [
+    pkgs.nodejs_22
+    pkgs.sudo
+  ];
+  idx.extensions = [
+    "svelte.svelte-vscode"
+    "vue.volar"
+  ];
+  idx.previews = {
+    previews = {
+      web = {
+        command = [
+          "npm"
+          "nvm"
+          "run"
+          "dev"
+          "--"
+          "--port"
+          "$PORT"
+          "--host"
+          "0.0.0.0"
+        ];
+        manager = "web";
+      };
+    };
+  };
+}
